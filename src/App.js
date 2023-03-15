c363e2d47972269a91afeced05a2a85b4a662992
Shankar <shankar.n@ethnus.com>
2023-03-15 21:08:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Codemithra
+          Learn MERN with Codemithra
         </a>
       </header>
     </div>
